937881997f6ce1c7b727381cbc388ad236fab570
Drilio <Roy-Antoine93@proton.me>
2024-02-21 14:47:36 +0100

    first commit

---
+++ Esport/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Esport
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Esport/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Esport
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
