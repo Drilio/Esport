2b9dba53f64c02dd74f91793f7c539f205eed2fa
Drilio <Roy-Antoine93@proton.me>
2024-02-22 12:05:45 +0100

    Creation de Service Title Game et Game avec interface

---
+++ Esport/Back/Service/CRUD/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esport.Back.Service.CRUD.DataTransferObject;
+using Esport.Interface;
+namespace Esport.Back.Service.CRUD
+{
+    internal class Game
+{
+    public List<ITitleGame> ListOfGamesTitles = new List<ITitleGame> { 
+        new TitleGame("League Of Legends"), 
+        new TitleGame("Overwatch"),
+        new TitleGame("JeuDuScam = Diablo 4") };
+
+    public List<ITitleGame> GetAllGamesTitles()
+        {
+            return ListOfGamesTitles;
+        }
+    }
+}
