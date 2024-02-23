a0bebbc35fc862acecad57a771ca4d9f6d6b04b9
Drilio <Roy-Antoine93@proton.me>
2024-02-23 09:21:39 +0100

    Implémentation du CRUD Game et du domain

--- Esport/Back/Domain/EGame.cs
+++ Esport/Back/Domain/EGame.cs
@@ -18,6 +18,15 @@ namespace Esport.Back.Domain
         public int WinnerTeamId { get; set; }
 
         public DateTime Date { get; set; }
+        //Constructeur
+        public EGame(int id, int IdVideoGame, List<int> PlayersId, List<int>TeamsId, int WinnerTeamId, DateTime Date) { 
+            this.Id = id;
+            this.IdVideoGame = IdVideoGame;
+            this.PlayersId = PlayersId;
+            this.TeamsId = TeamsId;
+            this.WinnerTeamId = WinnerTeamId;
+            this.Date = Date;
+        }
 
     }
 }
--- Esport/Back/Persistence/CRUD/PGames.cs
+++ Esport/Back/Persistence/CRUD/PGames.cs
@@ -11,29 +11,66 @@ namespace Esport.Back.Persistence.CRUD
     internal class PGames
     {
 
-        //public List<EGame> DummyGammes{ get; set; } = new List<EGame>()
-        //{
-        //    new ("jojo", "bobo", new Team(1, "JU"))
-        //};
         private EGame _gameToSave { get; set; }
+        private string filePath = "players.json";
 
+        //List de parties qui sera sauvegardé
+        public List<EGame> ListOfGames { get; set; } = new List<EGame>()
+        {
+            new EGame(1,2,new List<int>(){1,2,3,4,5,6,7, }, new List<int>(){1,2}, 2, DateTime.Now),
+            new EGame(2,1,new List<int>(){1,2,3,4,5,6,7, }, new List<int>(){2,3}, 3, DateTime.Now)
+        };
+
+        //Fonction qui appelle la fonction saveGameAsync et joue le rôle de l'async
         public void AddGame(EGame game)
         {
-            _gameToSave = game;
+            ListOfGames.Add(game);
 
             var t = SaveGameAsync().Result;
         }
-        public void CreateGame(EGame gameToSave)
+
+        //Fonction qui modifie la partie visé par l'ID
+        public void ModifyGame(int id, int idVideoGame, List<int> PlayersId, List<int> TeamsId, int winnerTeamId, DateTime TimeStampDeLaPartie)
         {
-            var newGame = ObjectSerializer.SerializeToFile(gameToSave, "C:\\Users\\drilio\\source\\repos\\Esport\\Esport\\Back\\DataBase"); 
+            foreach (var item in ListOfGames)
+            {
+                if (item.Id == id)
+                {
+                    item.IdVideoGame = idVideoGame;
+                    item.PlayersId = PlayersId;
+                    item.TeamsId = TeamsId;
+                    item.WinnerTeamId = winnerTeamId;
+                    item.Date = TimeStampDeLaPartie;
+                }
+            }
+            var t = SaveGameAsync().Result;
         }
 
-        private string filePath = "players.json";
+        //Fonction qui fournis la liste des parties de manière asynchrone
+        public async Task LoadPlayersAsync()
+        {
+            ListOfGames = await ObjectSerializer.DeserializeFromFile<List<EGame>>(filePath) ?? new List<EGame>();
+        }
 
+        /*Fonction Qui sauvegarde en asyncrone la sauvegarde et renvoie un bouléen pour que le reste de la stack s'execute une fois
+        la sauvegarde terminé */
         public async Task<bool> SaveGameAsync()
         {
             await ObjectSerializer.SerializeToFile(_gameToSave, filePath);
             return true;
         }
+        /*Suppression de la partie grace à son ID*/
+        public void RemoveGame(int id)
+        {
+            foreach (var item in ListOfGames)
+            {
+                if (item.Id == id)
+                {
+                    ListOfGames.Remove(item);
+                }
+            }
+        }
     }
+
+
 }
